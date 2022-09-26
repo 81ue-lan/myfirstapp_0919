de846545aea7db17c1549c0524d6c28c2a0f4814
lan <kitty90508s@gmail.com>
2022-09-26 14:20:35 +0800

    add pic

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   var appTitle = const Text('我的第一個Flutter App'),
       hiFlutter = const Text(
-        'Hi,Flutter.',
-        style: TextStyle(fontSize: 50),
+        'Hi,Flutter.'
+        '今天是20220926',
+        style: TextStyle(fontSize: 30,color: Colors.red,decoration: TextDecoration.underline),
       );
   //建立appBody物件
+  var img = Image.network("https://www.tku.edu.tw/images/logo_x2.jpg");
   var appBody = Center(
-    child: hiFlutter,
+    child: img
   );
   //建立appBar物件
   var appBar = AppBar(
     title: appTitle,
+    backgroundColor: Color(0xFF80DEEA),
   );
 
   //建立app物件
@@ -20,6 +23,7 @@ void main() {
     home: Scaffold(
       appBar: appBar,
       body: appBody,
+        backgroundColor: Colors.grey
     ),
   );
   runApp(app);
